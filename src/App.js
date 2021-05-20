fea8e43dd8e0654d1480913f9c59a9ec18e2c804
Elaman Imashov <elaman@outlook.com>
2021-05-20 15:16:06 +0600

    Add if authenticated

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useSelector } from "react-redux";
 import { Route, Switch } from "react-router";
 import Auth from "./components/Auth/Auth";
 import Home from "./components/Home/Home";
@@ -5,13 +6,23 @@ import Logout from "./components/Logout/Logout";
 import Nav from "./components/Nav/Nav";
 
 function App() {
+  const isAuthenticated = useSelector(state => state.auth.token !== null);
+
   let output = (
     <Switch>
       <Route path="/" component={Home} exact />
       <Route path="/auth" component={Auth} />
-      <Route path="/logout" component={Logout} />
     </Switch>
   );
+  if (isAuthenticated) {
+    output = (
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/auth" component={Auth} />
+        <Route path="/logout" component={Logout} />
+      </Switch>
+    );
+  }
 
   return (
     <div className="App">
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,11 +1,14 @@
+import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom"
 
 const Nav = () => {
+  const isAuthenticated = useSelector(state => state.auth.token !== null);
+
   return (
     <ul>
       <li><NavLink to="/" exact>Home</NavLink></li>
-      <li><NavLink to="/auth">Auth</NavLink></li>
-      <li><NavLink to="/logout">Logout</NavLink></li>
+      { !isAuthenticated ? <li><NavLink to="/auth">Auth</NavLink></li> : null }
+      { isAuthenticated ? <li><NavLink to="/logout">Logout</NavLink></li> : null }
     </ul>
   );
 }
