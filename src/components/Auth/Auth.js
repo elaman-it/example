e4e74c378e5cb86f7ef2ab9d9e51f5d0214f891a
Elaman Imashov <elaman@outlook.com>
2021-05-20 14:21:33 +0600

    Add new form

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,7 +1,17 @@
 const Auth = () => {
+  const submitCallback = (event) => {
+    event.preventDefault();
+  }
+
   return (
     <div>
       <h1>Welcome</h1>
+      <form onSubmit={submitCallback}>
+        <input type="email" name="email" placeholder="Email" required />
+        <input type="password" name="password" placeholder="Password" required />
+        <button>Sign in</button>
+        <button>Sign up</button>
+      </form>
     </div>
   );
 }
