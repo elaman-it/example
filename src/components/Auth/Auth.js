2eeaad7787e2ae4c964583df2b198f363be8bf3f
Elaman Imashov <elaman@outlook.com>
2021-05-20 14:59:21 +0600

    Add disptaches

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,6 +1,18 @@
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { auth } from "../../store/actions/auth";
+
 const Auth = () => {
+  const dispatch = useDispatch();
+  const error = useSelector(state => state.auth.error);
+  const [method, setMethod] = useState("signin");
+
   const submitCallback = (event) => {
     event.preventDefault();
+
+    const data = new FormData(event.target);
+
+    dispatch(auth(method, data.get('email'), data.get('password')));
   }
 
   return (
@@ -9,8 +21,8 @@ const Auth = () => {
       <form onSubmit={submitCallback}>
         <input type="email" name="email" placeholder="Email" required />
         <input type="password" name="password" placeholder="Password" required />
-        <button>Sign in</button>
-        <button>Sign up</button>
+        <button onClick={() => setMethod("signin")}>Sign in</button>
+        <button onClick={() => setMethod("signup")}>Sign up</button>
       </form>
     </div>
   );
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,12 @@
+import { useSelector } from "react-redux";
+
 const Home = () => {
+  const auth = useSelector(state => state.auth);
+
   return (
     <div>
+      {auth.token}
+      {auth.id}
       <h1>Home</h1>
     </div>
   );
